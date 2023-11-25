44a048d243e40f433b6614900585b5283877acf5
adamhenson <adamhenson1979@gmail.com>
2023-11-24 23:27:12 -0500

    test: playwright tests

--- tests/media-recorder.spec.ts
+++ tests/media-recorder.spec.ts
@@ -7,6 +7,7 @@ test.describe('MediaRecorder', async () => {
   // while (count <= MAX_LOOP_COUNT) {
   test(`recorded blob has a valid size (${count})`, async ({ page }) => {
     await page.goto('/');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     await page.screenshot({ path: 'playwright-report/screenshot.png' });
 
     const buttonStartStop = page.getByTestId('button-start-stop');
