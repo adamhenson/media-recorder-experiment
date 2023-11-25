f913d63eb13a59cf47cde94a8f66a6b3b30dd963
adamhenson <adamhenson1979@gmail.com>
2023-11-24 22:20:22 -0500

    test: playwright tests

--- tests/media-recorder.spec.ts
+++ tests/media-recorder.spec.ts
@@ -4,28 +4,28 @@ const MAX_LOOP_COUNT = 10;
 
 let count = 1;
 test.describe('MediaRecorder', async () => {
-  while (count <= MAX_LOOP_COUNT) {
-    test(`recorded blob has a valid size (${count})`, async ({ page }) => {
-      await page.goto('/');
+  // while (count <= MAX_LOOP_COUNT) {
+  test(`recorded blob has a valid size (${count})`, async ({ page }) => {
+    await page.goto('/');
 
-      const buttonStartStop = page.getByTestId('button-start-stop');
-      await buttonStartStop.click();
-      await expect(buttonStartStop).toHaveText('Stop');
+    const buttonStartStop = page.getByTestId('button-start-stop');
+    await buttonStartStop.click();
+    await expect(buttonStartStop).toHaveText('Stop');
 
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      await buttonStartStop.click();
-      await expect(buttonStartStop).toHaveText('Start');
+    await buttonStartStop.click();
+    await expect(buttonStartStop).toHaveText('Start');
 
-      const blobSize = page.getByTestId('blob-size');
-      await expect(blobSize).toBeVisible();
+    const blobSize = page.getByTestId('blob-size');
+    await expect(blobSize).toBeVisible();
 
-      const textContent = await blobSize.textContent();
-      const blobSizeNumber = Number(textContent);
-      expect(typeof blobSizeNumber).toBe('number');
-      expect(isNaN(blobSizeNumber)).toBe(false);
-      expect(blobSizeNumber).toBeGreaterThan(0);
-    });
-    count++;
-  }
+    const textContent = await blobSize.textContent();
+    const blobSizeNumber = Number(textContent);
+    expect(typeof blobSizeNumber).toBe('number');
+    expect(isNaN(blobSizeNumber)).toBe(false);
+    expect(blobSizeNumber).toBeGreaterThan(0);
+  });
+  count++;
+  // }
 });
